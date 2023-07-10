3ec7dba9ccd0fa8a02aabb214b929e7e9c7e57af
swarnya246 <swarnya246@gmail.com>
2023-07-09 20:36:42 -0700

    initial

--- pages/index.js
+++ pages/index.js
@@ -1,115 +1,71 @@
 import Head from 'next/head';
 import styles from '../styles/Home.module.css';
+import localFont from 'next/font/local'
 
 export default function Home() {
   return (
-    <div className={styles.container}>
+    <div>
       <Head>
-        <title>Create Next App</title>
+        <title>De Anza Hacks</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
       <main>
-        <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
+        <h1>
+          De Anza Hacks
         </h1>
+        <h2>
+          Cupertino's largest collegiate hackathon.
+        </h2>
 
-        <p className={styles.description}>
-          Get started by editing <code>pages/index.js</code>
+        <p>
+          Questions about the event or interest in sponsoring? 
+          <br />
+          Reach out at <a href="mailto:team@deanzahacks.com">team@deanzahacks.com</a>
+          <br /><br />
+          <span className="date"><b><i>October 21st and 22nd</i></b></span>
         </p>
 
-        <div className={styles.grid}>
-          <a href="https://nextjs.org/docs" className={styles.card}>
-            <h3>Documentation &rarr;</h3>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
-
-          <a href="https://nextjs.org/learn" className={styles.card}>
-            <h3>Learn &rarr;</h3>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/master/examples"
-            className={styles.card}
-          >
-            <h3>Examples &rarr;</h3>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
-
-          <a
-            href="https://vercel.com/import?filter=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-          >
-            <h3>Deploy &rarr;</h3>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
+        <a href="https://forms.gle/e6DMCyD4j4ivxs7f9" target="_blank" rel="noopener noreferrer">
+          <button>
+            Register now!
+          </button>
+        </a>
+        <p></p>
+        <h3>Frequently Asked Questions</h3>
+        <div className="faq">
+          <div className="qa">
+            <b>What's a hackathon?</b>
+            <p>A hackathon is an educational event where coders can create new things—you can make apps, websites, hardware hacks, etc.</p>
+          </div>
+          <div className="qa">
+            <b>Who can participate?</b>
+            <p>Any college student can participate. High school students and recent grads are not permitted.</p>
+          </div>
+          <div className="qa">
+            <b>How do I join a team?</b>
+            <p>You can form your own team of four, or you can join us at a team mixer at the beginning of the hackathon and meet others to form a team.</p>
+          </div>
+          <div className="qa">
+            <b>Does this cost money?</b>
+            <p>No! Thanks to the generous support of our sponsors, De Anza Hacks is free for everyone! Food, swag, and other costs are covered.</p>
+          </div>
+          <div className="qa">
+            <b>What should I bring?</b>
+            <p>Bring a valid student ID, a laptop, and chargers. Power strips, sleeping bags, and toiletries are highly recommended.</p>
+          </div>
+          <div className="qa">
+            <b>What if I can't code?</b>
+            <p>No problem! We are accepting people regardless of experience. We will have workshops where you can learn.</p>
+          </div>
         </div>
       </main>
 
       <footer>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <img src="/vercel.svg" alt="Vercel" className={styles.logo} />
-        </a>
+        © 2023 De Anza Hacks.
+        <br />
+        Fiscally sponsored by The Hack Foundation. 
       </footer>
-
-      <style jsx>{`
-        main {
-          padding: 5rem 0;
-          flex: 1;
-          display: flex;
-          flex-direction: column;
-          justify-content: center;
-          align-items: center;
-        }
-        footer {
-          width: 100%;
-          height: 100px;
-          border-top: 1px solid #eaeaea;
-          display: flex;
-          justify-content: center;
-          align-items: center;
-        }
-        footer img {
-          margin-left: 0.5rem;
-        }
-        footer a {
-          display: flex;
-          justify-content: center;
-          align-items: center;
-          text-decoration: none;
-          color: inherit;
-        }
-        code {
-          background: #fafafa;
-          border-radius: 5px;
-          padding: 0.75rem;
-          font-size: 1.1rem;
-          font-family: Menlo, Monaco, Lucida Console, Liberation Mono,
-            DejaVu Sans Mono, Bitstream Vera Sans Mono, Courier New, monospace;
-        }
-      `}</style>
-
-      <style jsx global>{`
-        html,
-        body {
-          padding: 0;
-          margin: 0;
-          font-family: -apple-system, BlinkMacSystemFont, Segoe UI, Roboto,
-            Oxygen, Ubuntu, Cantarell, Fira Sans, Droid Sans, Helvetica Neue,
-            sans-serif;
-        }
-        * {
-          box-sizing: border-box;
-        }
-      `}</style>
     </div>
   )
 }
